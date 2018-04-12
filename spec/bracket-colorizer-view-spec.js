15181ca09fefb0b3e2b0f40e1e7c343b73f7baee
Tony Brix <tony@brix.ninja>
2018-04-11 23:25:15 -0500

    remove unused files

--- spec/bracket-colorizer-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import BracketColorizerView from '../lib/bracket-colorizer-view';
-
-describe('BracketColorizerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
